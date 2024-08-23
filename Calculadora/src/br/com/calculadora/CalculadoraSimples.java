9baceaba92cedb15c85ab750c952f5af98ab4b6e
wenze <wenze@DESKTOP-D5BIGD0>
2024-08-23 00:22:26 -0300

    Primeiro commit

---
+++ Calculadora/src/br/com/calculadora/CalculadoraSimples.java
@@ -0,0 +1,10 @@
+package br.com.calculadora;
+
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+        int numero1 = 314;
+        int numero2 = 141;
+        int soma = numero1 + numero2;
+        System.out.println("A soma de " + numero1 + " e " + numero2 + " é " + soma);
+    }
+}
